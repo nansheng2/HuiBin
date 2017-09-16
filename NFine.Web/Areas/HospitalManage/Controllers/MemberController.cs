791c2b5ce675109513b3409cd875c7fa21559a3a
nansheng2 <270949689@qq.com>
2017-09-16 11:29:45 +0800

    添加

---
+++ NFine.Web/Areas/HospitalManage/Controllers/MemberController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NFine.Web.Areas.HospitalManage.Controllers
+{
+    public class MemberController : ControllerBase
+    {
+    }
+}
