70ee28a71a5cc8542f143d552cccee71869078d1
nansheng2 <270949689@qq.com>
2017-09-19 23:59:17 +0800

    修改医生出诊安排

---
+++ NFine.Application/SystemManage/DoctorApp.cs
@@ -0,0 +1,49 @@
+﻿using NFine.Code;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NFine.Application.SystemManage
+{
+    public class DoctorApp
+    {
+        private IRoleRepository service = new RoleRepository();
+        private ModuleApp moduleApp = new ModuleApp();
+        private ModuleButtonApp moduleButtonApp = new ModuleButtonApp();
+
+        public void SubmitForm(RoleEntity roleEntity, string[] permissionIds, string keyValue)
+        {
+            if (!string.IsNullOrEmpty(keyValue))
+            {
+                roleEntity.F_Id = keyValue;
+            }
+            else
+            {
+                roleEntity.F_Id = Common.GuId();
+            }
+            var moduledata = moduleApp.GetList();
+            var buttondata = moduleButtonApp.GetList();
+            List<RoleAuthorizeEntity> roleAuthorizeEntitys = new List<RoleAuthorizeEntity>();
+            foreach (var itemId in permissionIds)
+            {
+                RoleAuthorizeEntity roleAuthorizeEntity = new RoleAuthorizeEntity();
+                roleAuthorizeEntity.F_Id = Common.GuId();
+                roleAuthorizeEntity.F_ObjectType = 1;
+                roleAuthorizeEntity.F_ObjectId = roleEntity.F_Id;
+                roleAuthorizeEntity.F_ItemId = itemId;
+                if (moduledata.Find(t => t.F_Id == itemId) != null)
+                {
+                    roleAuthorizeEntity.F_ItemType = 1;
+                }
+                if (buttondata.Find(t => t.F_Id == itemId) != null)
+                {
+                    roleAuthorizeEntity.F_ItemType = 2;
+                }
+                roleAuthorizeEntitys.Add(roleAuthorizeEntity);
+            }
+            service.SubmitForm(roleEntity, roleAuthorizeEntitys, keyValue);
+        }
+    }
+}
---
+++ NFine.Domain/02 ViewModel/DoctorViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NFine.Domain._02_ViewModel
+{
+    /// <summary>
+    /// 医生
+    /// </summary>
+    public class DoctorViewModel
+    {
+
+    }
+}
